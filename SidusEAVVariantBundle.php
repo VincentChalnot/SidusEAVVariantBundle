3cddf03c49266fed5e30925e502bab1a1683d9fe
Vincent Chalnot <vchalnot@clever-age.com>
2017-02-28 19:09:18 +0100

    Fixed template inheritance bug with symfony upgrade

--- SidusEAVVariantBundle.php
+++ SidusEAVVariantBundle.php
@@ -6,13 +6,4 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class SidusEAVVariantBundle extends Bundle
 {
-    /**
-     * Used to override family parsing
-     *
-     * @return string
-     */
-    public function getParent()
-    {
-        return 'SidusEAVBootstrapBundle';
-    }
 }
